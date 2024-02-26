5f9315a6be36acc5fb6687bfa55c63e6eb5e3dfc
Irbaz2004 <irbazahmeds20@gmail.com>
2024-02-26 15:51:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+ 
+let submit=document.getElementById("submit");
+let input= document.getElementById("mail")
+submit.addEventListener("click",()=>{
+    if (input.value !== "") {
+        alert("Thank You for Contact!")
+        
+     } 
+     else{
+        alert("Please fill the details")
+     } 
+})
+ 
